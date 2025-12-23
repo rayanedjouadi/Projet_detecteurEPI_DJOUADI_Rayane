8628051292554b952d2f9a54683329cfef3d8ec2
Rayane Djouadi <164172917+rayanedjouadi@users.noreply.github.com>
2025-12-23 20:02:16 +0100

    Update final_demo.py

--- final_demo.py
+++ final_demo.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 import cv2
 
-# Charge ton modèle fraîchement entraîné
-# Remplace le chemin si tu n'as pas déplacé le fichier
+# Charge le modèle fraîchement entraîné
+# Remplacez le chemin si vous n'avez pas déplacé le fichier
 model = YOLO('best.pt') 
 
 # Utilise l'index de caméra qui fonctionnait lors de tes tests
@@ -11,7 +11,7 @@ cap = cv2.VideoCapture(0)
 while cap.isOpened():
     success, frame = cap.read()
     if success:
-        # L'IA analyse avec ton modèle "best.pt"
+        # L'IA analyse avec le modèle "best.pt"
         results = model(frame, conf=0.5) # On n'affiche que si l'IA est sûre à 50%
         annotated_frame = results[0].plot()
         
